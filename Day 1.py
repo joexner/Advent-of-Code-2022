5ab1832bf7ccaecc95a1098b33e32f85a635d41a
Jonathan Oexner <jonathan.oexner@alum.wpi.edu>
2022-12-01 00:20:58 -0500

    Day 1, p1

---
+++ Day 1.py
@@ -0,0 +1,15 @@
+from locale import atoi
+
+filename = 'Day 1 - input.txt'
+groups = []
+with open(filename) as file:
+    group_value = 0
+    for line in file:
+        line = line.strip()
+        if len(line) > 0:
+            group_value += atoi(line)
+        else:
+            groups.append(group_value)
+            group_value = 0
+
+print(max(groups))
