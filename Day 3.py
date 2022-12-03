20daefcf229add736f017f6b50b88ad9b240f06c
Jonathan Oexner <jonathan.oexner@alum.wpi.edu>
2022-12-03 01:43:54 -0500

    Day 3, pt 2

--- Day 3.py
+++ Day 3.py
@@ -17,3 +17,31 @@ with open(filename) as file:
 
     print(total_shared_item_values)
 
+with open(filename) as file:
+    total_shared_item_values = 0
+    group_size = 0
+    group_intersection = None
+    file.__iter__()
+    for line in file:
+        bag_contents = set(line.strip())
+        if group_intersection is None:
+            group_intersection = bag_contents
+        else:
+            group_intersection = group_intersection & bag_contents
+        group_size += 1
+        if group_size == 3:
+            (shared_item, ) = group_intersection
+            shared_item_value = ord(shared_item) - ord('A')
+            if shared_item_value > 32:  # lower-case
+                shared_item_value -= 31
+            else:
+                shared_item_value += 27
+            total_shared_item_values += shared_item_value
+
+            # Reset group stats
+            group_size = 0
+            group_intersection = None
+
+
+    print(total_shared_item_values)
+
